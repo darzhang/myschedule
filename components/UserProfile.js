4d47f0df372d3e32e0aed3928b55288fa3381555
Darren Zhang <darzhang@student.unimelb.edu.au>
2022-10-16 13:42:08 +1100

    create user profile component to display user data field

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,11 +1,22 @@
-import { useAuth } from "../context/AuthContext"
+import { Box, TextField, Typography } from "@mui/material";
 
-export default function UserProfile() {
-  const {user} = useAuth();
+export default function UserProfile({doc}) {
+  
+
   return (
-    <>
-      <div>UserProfile</div>
-      <div>{user.email}</div>
-    </>
+    <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <Typography variant="h3">User Profile</Typography>
+      {Object.keys(doc).map((key,index) => {
+        return (
+          <div key={index}>
+            <TextField
+              sx={{m: "10px"}}
+              label={key}
+              defaultValue={doc[key]} 
+              inputProps={{readOnly: true}}/>
+          </div>
+        )
+      })}
+    </Box>
   )
 }
