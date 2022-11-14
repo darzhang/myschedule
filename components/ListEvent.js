13a001d7b564cf00b1167d0b8662043af9c26c65
Darren Zhang <darzhang@student.unimelb.edu.au>
2022-11-14 21:25:46 +1100

    change the event list item on schedule page to show start and end time instead of description and make the event item clickable

--- components/ListEvent.js
+++ components/ListEvent.js
@@ -4,29 +4,32 @@ import {
   IconButton,
   List,
   ListItem,
+  ListItemButton,
   ListItemText,
 } from "@mui/material";
 import { Box } from "@mui/system";
+import moment from "moment";
+import { useRouter } from "next/router";
 
 export default function ListEvent({ events, handleDelete }) {
+  const router = useRouter();
   return (
     <Box>
       <List>
         {events.map((event, index) => {
           return (
             <Box key={index}>
-              <ListItem>
+              <ListItemButton>
                 <ListItemText
                   primary={event.title}
-                  secondary={event.description}
+                  secondary={`${moment(event.start).format(
+                    "DD/mm/YYYY h:mm a"
+                  )} - ${moment(event.end).format("DD/mm/YYYY h:mm a")}`}
                 />
-                <IconButton>
-                  <EditOutlined />
-                </IconButton>
                 <IconButton onClick={() => handleDelete(event.id, event.title)}>
                   <DeleteOutline />
                 </IconButton>
-              </ListItem>
+              </ListItemButton>
               <Divider />
             </Box>
           );
