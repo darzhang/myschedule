1723886501492c5e1d58a00b9e0b47a37e2002e6
Darren Zhang <darzhang@student.unimelb.edu.au>
2022-11-29 22:34:41 +1100

    change the theme to have blueGrey[500] as the primary color and blue[500] as the secondary color

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import { blue, blueGrey, indigo } from "@mui/material/colors";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 import ProtectedRoute from "../components/ProtectedRoute";
@@ -7,20 +10,33 @@ import "../styles/react-big-calendar.css";
 
 const noAuthRequired = ["/", "/signin", "/signup", "/schedule/[uid]"];
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blueGrey[500],
+    },
+    secondary: {
+      main: blue[500],
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
-    <AuthContextProvider>
-      <Layout>
-        {noAuthRequired.includes(router.pathname) ? (
-          <Component {...pageProps} />
-        ) : (
-          <ProtectedRoute>
+    <ThemeProvider theme={theme}>
+      <AuthContextProvider>
+        <Layout>
+          {noAuthRequired.includes(router.pathname) ? (
             <Component {...pageProps} />
-          </ProtectedRoute>
-        )}
-      </Layout>
-    </AuthContextProvider>
+          ) : (
+            <ProtectedRoute>
+              <Component {...pageProps} />
+            </ProtectedRoute>
+          )}
+        </Layout>
+      </AuthContextProvider>
+    </ThemeProvider>
   );
 }
 
